af82c769aed822d81917abd2ac0b8558ecbda5ab
Manu <aglasnooze@gmail.com>
2025-10-26 18:00:13 -0300

    conclusao preparado p deploy no vercel

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ server.get('/horaAtual', (req, res) => {
     res.send('Hora atual: ' + hora);
 });
 
+//criar um metodo que aceita parametro
+server.get('/tabuada',(req, res) => {
+    //tabuada de qual numero e ate qual sequencia?
+    const numero = parseInt(req.query.numero);
+    const sequencia = parseInt(req.query.sequencia);
+    if(!numero || !sequencia){
+        res.send('Por favor, informe o numero e a sequencia na URL <br> Exemplo: /tabuada?numero=5&sequencia=10');
+    }else{
+        res.setHeader('Content-Type', 'text/html');//definindo o tipo de conteudo que sera enviado
+        res.write(`Tabuada do ${numero} ate a ${sequencia}: `);
+
+        for(let i = 1; i <= sequencia; i++){
+            res.write(`<br> ${numero} x ${i} = ${numero * i}`);
+        }
+
+        res.end();//finaliza o envia
+    }
+    console.log("requisicao tabuada");
+});
+
 server.listen(porta, host, () => {
     console.log(`Servidor rodando em http://${host}:${porta}`);
 });
